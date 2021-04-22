46d919b4393eff63b294a11ab3e32a8787cbdea1
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-04-22 11:52:34 -0400

    Fix issue with editing user role in case

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MotionToDismissCRA',
-      version='0.0.2',
+      version='0.0.3',
       description=('Motion to dismiss Child Requiring Assistance Case'),
       long_description='# docassemble.MotionToDismissCRA\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nCaroline Robinson, crobinson@mlri.org\r\n\r\n',
       long_description_content_type='text/markdown',
